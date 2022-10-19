6b5d98d5a808e226730bc8d2173369367425ccf4
dhyanjyotiprati123 <dhyanjyoti123@gmail.com>
2022-10-19 22:16:56 +0530

    chod

--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -9,7 +9,7 @@ const Customer = () => {
      allowEditing: true
   }
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-2 md:m-10 p-2 md:p-10 mt-24 bg-white rounded-3xl'>
     <Header title="Customers" category="Page" />
 
     <GridComponent
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -5,7 +5,7 @@ import {employeesData, employeesGrid} from "../data/dummy"
 
 const Employee = () => {
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
         <Header title="Employee" category="Page" />
 
         <GridComponent
